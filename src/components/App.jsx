e8424f0bb99876cdba8574c1e5f46f73cc9143ef
Sadim Mali <sadimmalakar77@gmail.com>
2023-04-30 13:01:55 +0545

    Created and added CreateArea and Note components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,10 @@
 import { useState } from 'react'
 // import reactLogo from './assets/react.svg'
 import Header from "./Header";
+import CreateArea from './CreateArea';
+import Note from './Note';
 import Footer from "./Footer";
+
 
 function App() {
  
@@ -9,6 +12,8 @@ function App() {
   return (
    <div>
     <Header />
+    <CreateArea />
+    <Note />
     <Footer />
    </div>
   )
