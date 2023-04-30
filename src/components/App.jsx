6fd798681ff4da5c564cb8f061886e7e10395531
Sadim Mali <sadimmalakar77@gmail.com>
2023-04-30 12:52:32 +0545

    first commit

---
+++ src/components/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+import Header from "./Header";
+import Footer from "./Footer";
+
+function App() {
+ 
+
+  return (
+   <div>
+    <Header />
+    <Footer />
+   </div>
+  )
+}
+
+export default App
